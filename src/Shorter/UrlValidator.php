5bafc6c033109b696325d442e7d0e793dcd91d75
vitaliy <blazkoit@gmail.com>
2023-03-29 21:49:40 +0300

    init

--- src/Shorter/UrlValidator.php
+++ src/Shorter/UrlValidator.php
@@ -8,13 +8,13 @@ use InvalidArgumentException;
 
 class UrlValidator
 {
-
+    
     public function validate(string $url): void
     {
-//        // Validate the URL
-//        if (!filter_var($url, FILTER_VALIDATE_URL)) {
-//            throw new InvalidArgumentException('Invalid URL');
-//        }
+        // Validate the URL
+        if (!filter_var($url, FILTER_VALIDATE_URL)) {
+            throw new InvalidArgumentException('Invalid URL');
+        }
 
 
     }
