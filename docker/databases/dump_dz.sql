f9f537af3f45d909983c9eeb2ec8630c961d05ef
vitaliy <blazkoit@gmail.com>
2023-05-15 17:00:22 +0300

    init Shortner_in_DB

---
+++ docker/databases/dump_dz.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `code_url` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `code` varchar(45) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
